325b3a311e8e12a7b07d786473cab4fab264164b
majdsassi <jackreider997@gmail.com>
2025-11-09 00:16:05 +0100

    Create Login Handler

---
+++ handlers/loginHandler.php
@@ -0,0 +1,29 @@
+<?php
+require_once '../config.php';
+session_start();
+
+if (!empty($_POST)) {
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+        try {
+            $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+            $stmt = $pdo->prepare("SELECT * FROM users WHERE username = ?");
+            $stmt->execute([$_POST['username']]);
+            $user = $stmt->fetch(PDO::FETCH_OBJ);
+            
+            // Verify user password and set $_SESSION
+            if ($user && password_verify($_POST['password'], $user->password)) {
+                $_SESSION['user_id'] = $user->ID;
+            }
+            header("Location : ".DOAMAIN."login.php") ; 
+
+            
+        } catch (PDOException $e) {
+            error_log("Database error: " . $e->getMessage());
+            header("Location : ".DOAMAIN."login.php") ; 
+        }
+    }
+}
+header("Location : ".DOAMAIN."login.php") ; 
+?>
