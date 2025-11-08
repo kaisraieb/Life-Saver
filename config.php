15e6f28a7fcaaef86b6991a52ce8d0ebd3386af5
majdsassi <jackreider997@gmail.com>
2025-11-09 00:53:54 +0100

    Rename Config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// Paramètres de connexion MySQL
+define('DB_HOST', 'localhost'); // Hôte (souvent localhost)
+define('DB_NAME', 'lifesaver'); // Nom de la base de données créée
+define('DB_USER', 'root');       // Utilisateur par défaut
+define('DB_PASS', '');           // Mot de passe par défaut (souvent vide)
+define('DOMAIN','localhost/Blood-Donation/') ;
+// Creds for testing purpose only y3nny 3adi ki pushihom 3al git 
+?>
