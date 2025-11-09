ed7201dabd1379ae316eb4237af3a213b6a60b30
majdsassi <jackreider997@gmail.com>
2025-11-09 13:12:39 +0100

    Update Config.php

--- config.php
+++ config.php
@@ -4,6 +4,6 @@ define('DB_HOST', 'localhost'); // Hôte (souvent localhost)
 define('DB_NAME', 'lifesaver'); // Nom de la base de données créée
 define('DB_USER', 'root');       // Utilisateur par défaut
 define('DB_PASS', '');           // Mot de passe par défaut (souvent vide)
-define('DOMAIN','localhost');
+define('DOMAIN','http://192.168.56.101:8080/');
 // Creds for testing purpose only y3nny 3adi ki pushihom 3al git 
 ?>
