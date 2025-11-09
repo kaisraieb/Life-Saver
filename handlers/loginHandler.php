cea3cda6c63a97ed7e65ac97d0d58242eb28e1ee
majdsassi <jackreider997@gmail.com>
2025-11-09 14:08:17 +0100

    Add Session Variables

--- handlers/loginHandler.php
+++ handlers/loginHandler.php
@@ -13,8 +13,9 @@ if (!empty($_POST)) {
             
             // Verify user password and set SESSION
             if ($user && password_verify($_POST['password'], $user["mot_de_passe"])) {
-                $_SESSION['user_id'] = $user["id"];
-                header("Location:".DOMAIN."index.php");
+                $_SESSION['user_id'] = $user["id_utilisateur"];
+                $_SESSION['user_role'] = $user["role"] ; 
+                header("Location:".DOMAIN."index.php?message=".$_SESSION["user_role"]);
                 exit;
             } else {
                 // Creds Ghalta 
