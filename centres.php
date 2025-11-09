15a7223b5bac499a803eb5a0e95880c0c8bf0a26
majdsassi <jackreider997@gmail.com>
2025-11-10 00:58:04 +0100

    remove include

--- centres.php
+++ centres.php
@@ -1,4 +1,20 @@
-<?php require_once "./utils/connection.php" ;?>
+<?php require_once "config.php" ;
+try {
+    // 1. Définition de la chaîne de connexion (DSN)
+    // mysql:host=...;dbname=...
+    $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME;
+
+    // 2. Création de l'objet PDO
+    $pdo = new PDO($dsn, DB_USER, DB_PASS);
+
+    // 3. Configuration importante : Afficher les exceptions/erreurs SQL
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    // Gestion de l'erreur
+    die("Erreur de connexion : " . $e->getMessage());
+}
+?>
 <body>
 <?php include "/includes/header.php"; ?>
 <div class="container">
