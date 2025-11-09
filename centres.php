94d7e6d3a4c28abd6bb8ac461879ee1f9f5a3efd
majdsassi <jackreider997@gmail.com>
2025-11-09 16:33:34 +0100

    Fix Relative Paths

--- centres.php
+++ centres.php
@@ -1,10 +1,10 @@
 <?php
 require_once "config.php";
-require_once __DIR__ . '/utils/connection.php';
-include "/includes/head.php"; 
+require_once __DIR__ . "/utils/connection.php";
+include __DIR__ . "/includes/head.php"; 
 ?>
 <body>
-<?php include "./includes/header.php"; ?>
+<?php include __DIR__ . "/includes/header.php"; ?>
 <div class="container">
     <h1 class="section-title">Centers</h1>
     <div class="row">
@@ -14,8 +14,8 @@ try {
     $stmt->execute();
     $centres = $stmt->fetchAll(PDO::FETCH_ASSOC);
     
-    if(count($centres) > 0) {
-        foreach($centres as $centre) {
+    if (count($centres) > 0) {
+        foreach ($centres as $centre) {
             echo "<div class='col-lg-3 col-md-6 mb-4'>
                     <div class='card'>
                         <img class='card-img-top' src='./images/hospital.png' alt='" . htmlspecialchars($centre['nom_centre']) . "'>
@@ -37,7 +37,7 @@ try {
 ?>
     </div>
 </div>
-<?php include "./includes/footer.php"; ?> 
+<?php include __DIR__ . "/includes/footer.php"; ?> 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
