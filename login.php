b3cbb7573ddc72c6358c5060998ce3105f5bf704
majdsassi <jackreider997@gmail.com>
2025-11-09 17:16:10 +0100

    Add New Type of errors

--- login.php
+++ login.php
@@ -4,6 +4,8 @@ $errors = [
     "invalid_credentials" => "Username Or Password is incorrect",
     "missing_fields" => "Don't Send Empty fields!!",
     "database_error" => "Error 500",
+    "403" => "403 Forbidden" ,
+    "401" => "401 Unauthorized",
     "access_report" => "You Try To access The Handler Directly That's Forbidden a Report will be sent , Your IP : ".$_SERVER['REMOTE_ADDR']
 ];
 ?> 
