f02a7a41297ff76b37a03357545124f8c848709c
majdsassi <jackreider997@gmail.com>
2025-11-18 15:43:44 +0100

    Fix Icons

--- medecin/index.php
+++ medecin/index.php
@@ -14,8 +14,8 @@ if(isset($_SESSION["user_id"])){
         <div class="col-md-3 mb-3">
             <div class="card shadow-sm border-0">
                 <div class="card-body text-center">
+                    <i class="bi-people fs-5" style = "color: #0d6efd;"></i>
                     <h5 class="card-title">Donneurs</h5> 
-                    <i class="bi-people"></i>
                     <h2 class="text-primary">
                         <?php
                             $stmt = $pdo->query("SELECT COUNT(*) FROM donneurs ");
@@ -30,8 +30,8 @@ if(isset($_SESSION["user_id"])){
         <div class="col-md-3 mb-3">
             <div class="card shadow-sm border-0">
                 <div class="card-body text-center">
+                    <i class="bi-check-circle fs-5" style="color : #198754 ;"></i>
                     <h5 class="card-title">Dons valides</h5>
-                    <i class="bi-check-circle"></i>
                     <h2 class="text-success">
                         <?php
                             $stmt = $pdo->prepare("SELECT COUNT(*) FROM dons WHERE statut='VALIDE'  AND `id_centre`= ?");
