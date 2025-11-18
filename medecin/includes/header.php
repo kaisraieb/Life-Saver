70bf5d9082fbcc2b53f5020a3e3f3d172dbe8a0f
majdsassi <jackreider997@gmail.com>
2025-11-18 15:58:17 +0100

    Return The Orginal Style

--- medecin/includes/header.php
+++ medecin/includes/header.php
@@ -10,7 +10,7 @@
 
     <!-- Bootstrap Icons-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
-    <link href="./styles/styles.css" rel="stylesheet">
+    <link href="../styles/styles.css" rel="stylesheet">
     
 
 </head>
